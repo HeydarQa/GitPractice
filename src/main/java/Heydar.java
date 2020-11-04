ccfd35ecc7ff699d354c3097c9aceaea12aca8ee
HeydarQa <qahaydar@gmail.com>
2020-11-03 23:36:08 -0500

    Heydar's commit

---
+++ src/main/java/Heydar.java
@@ -0,0 +1,7 @@
+public class Heydar {
+    public static void main(String[] args) {
+        System.out.println("Hello Word from Heydar");
+
+
+    }
+}
--- src/main/java/Said.java
+++ src/main/java/Said.java
@@ -1,6 +1,7 @@
 public class Said {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World from Said's branch");
     }
 }
